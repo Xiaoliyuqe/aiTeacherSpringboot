ce2841ad01318a5d4503246964ca77f24cbbb3a0
Xiaoliyuqe <1003236952@qq.com>
2023-05-31 22:22:17 +0800

    first

---
+++ src/main/java/com/example/myaiteacher/MyAiTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.example.myaiteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAiTeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAiTeacherApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/myaiteacher/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.myaiteacher.controller;
+
+public class UserController {
+}
